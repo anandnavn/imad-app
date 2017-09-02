86ecd12cc283e8bbb9e00306637b4df14e14b378
Navneet Anand <1629076@kiit.ac.in>
2017-09-02 12:16:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,23 +19,38 @@ button.onclick = function () {
 };
 
     //Make the request
-    request.open('GET','http://u1629076.imad.hasura-app.io/counter' , true);
+    request.open('GET','http://u1629076.hasura-app.io/counter' , true);
     request.send(null);
 };
  
   //submit name
-  var nameInput = document.getElementById('name');
-  var name = nameInput.value;
+ 
   var submit = document.getElementById('submit_btn');
   submit.onclick = function () {
       // Make a Request to the server and send the name
-       
+      var request = new XMLHttpRequest();
+      
       //capture a list of name and render it as a list
-      var names = ['name1', 'name2', 'name3'];
-      var list = '';
-      for (var i =0; i<names.length; i++) {
+      request.onreadystatechange = functionn () {
+          if (request.readystate === XMLHttpRequest.DONE)
+           //take some action
+           if(request.status ===200) {
+               
+               //capture a list of names and render it as a list
+               var names = request.responseText;
+               names = JSON.parse(names);
+               var list=''
+            for (var i =0; i<names.length; i++) {
           list == '<li>' + names[i] + '<li>';
       }
        var ul =  document.getElementById('namelist');
        ul.innerHTML = list;
+           }
+      }
   };
+  
+  //make request 
+   var nameInput = document.getElementById('name');
+   var name = nameInput.value;
+    request.open('GET','http://u1629076.imad.hasura-app.io/submit-name?name' +name ,true);
+    request.send(null);
